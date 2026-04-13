8cc68bb2ef226e652d6da405e1e856c1061dc79c
LilvanZetaZeta <iv.zamoranov@duocuc.cl>
2026-04-13 16:36:10 -0400

    implementacion de Typescript y Redux

---
+++ src/modules/todo/view/TodoView.tsx
@@ -0,0 +1,21 @@
+import { useTodoViewModel } from "../viewmodel/useTodoViewModel";
+
+export default function TodoView() {
+    const { todos, text, setText, createTodo, deleteTodo } = useTodoViewModel();
+
+    return (
+        <div>
+            <h1>Lista de Tareas</h1>
+            <input value={text} onChange={(e) => setText(e.target.value)}/>
+            <button onClick={createTodo}>Agregar</button>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>
+                        {todo.text}
+                        <button onClick={() => deleteTodo(todo.id)}>X</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
---
+++ src/modules/todo/viewmodel/useTodoViewModel.ts
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo, removeTodo } from "../todoSlice";
+import {todoService} from "../service/todoService";
+
+interface Todo {
+    id: string;
+    text: string;
+}
+
+interface TodoState {
+    todos: Todo[];
+}
+
+interface RootState {
+    todo: TodoState;
+}
+
+export const useTodoViewModel = () => {
+    const dispatch = useDispatch();
+    const todos = useSelector((state: RootState) => state.todo.todos);
+
+    const [text, setText] = useState("");
+
+    const createTodo = () => {
+        if (!text) return;
+
+        const newTodoRaw = todoService.create(text);
+        const newTodo = { ...newTodoRaw, id: String(newTodoRaw.id) };
+        dispatch(addTodo(newTodo));
+        setText("");
+    };
+
+    const deleteTodo = (id: string) => {
+        dispatch(removeTodo(id));
+    };
+
+    return {
+        todos,
+        text,
+        setText,
+        createTodo,
+        deleteTodo,
+    };
+};
